1fafee217acd9ac162231076df7c82542893d372
aminshahein <109917781+aminshahein@users.noreply.github.com>
2022-12-15 12:46:42 +0200

    Project

---
+++ js/main.js
@@ -0,0 +1,16 @@
+
+let toggleButton =document.querySelector(".toggle-button");
+let navbarLinks=document.querySelector(".navbar-links");
+
+toggleButton.addEventListener('click', ()=>{
+
+
+    navbarLinks.classList.toggle ('active')
+})
+
+$(document).ready(function(){
+    $('.your-class').slick({
+    
+    });
+  });
+      
